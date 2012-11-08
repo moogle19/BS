10573096a81c64e9cabbd1f05de77f4e05e96c4b
Kevin Seidel <kevinseidel@ubuntu.(none)>
2012-11-08 19:38:55 +0100

    Output of all needed things

--- A2/memusage.C
+++ A2/memusage.C
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 				}
 				else
 				{
-					printf("%ld\n", nd);
+					//printf("%ld\n", nd);
 				}
 				nc = 0;
 			}
@@ -55,11 +55,19 @@ int main(int argc, char** argv)
 			}
 		}
 	}
+
+
+
+
 	DIR* procdir;
 	FILE* cmd;
 	//int cmd;
-	FILE* stats;
-	char* path;
+	FILE* status;
+	char path[64];
+	char cmdpath[64];
+	char statuspath[64];
+	char cmdline[TEXTBUF];
+	char statusline[TEXTBUF];
 	char buffer[TEXTBUF];
 	struct dirent* procpoint;
 
@@ -75,11 +83,18 @@ int main(int argc, char** argv)
 		//puts(procpoint->d_name);
 		if(strtol(procpoint->d_name, NULL, 10) > 0)
 		{
-			//puts(procpoint->d_name);
-			//path = strcat("/proc/", procpoint->d_name);
-			//printf("%s\n",path);
-			//puts(path);
-			cmd = fopen("/proc/32477/cmdline", "r");
+			
+			//get path
+			strcpy(path, "/proc/");
+			strcat(path, procpoint->d_name);
+			strcpy(cmdpath, path);
+			strcpy(statuspath, path);
+
+			//cmdline
+			strcat(cmdpath, "/cmdline");
+			
+			//open cmdline file
+			cmd = fopen(cmdpath, "r");
 			if(cmd == NULL)
 			{
 				puts("Nicht geöffnet!!!");
@@ -88,10 +103,38 @@ int main(int argc, char** argv)
 			{
 				if((fgets(buffer, TEXTBUF, cmd)) != NULL)
 				{ 
-					puts(buffer);
+					strcpy(cmdline, buffer);
 				}
 			}
 			fclose(cmd);
+
+			puts(cmdline);
+
+			//other stats
+
+			strcat(statuspath, "/status");
+
+			status = fopen(statuspath, "r");
+			if(status == NULL)
+			{
+				fprintf(stderr, "%s\n", "Error in status path!");
+				return -1;
+			}
+			while(! feof(status))
+			{
+				if((fgets(buffer, TEXTBUF, status)) != NULL)
+				{
+					strcpy(statusline, buffer);
+					if(strncmp(statusline, "VmSize", 5) == 0)
+					{
+						puts(statusline);
+					}
+					if(strncmp(statusline, "State", 5) == 0)
+					{
+						puts(statusline);
+					}
+				}
+			}
 
 		}	
 	}
