7f2238c03d4877d470f2329dabbd9d5bb916941e
moogle19 <moogle.seidel@gmail.com>
2012-12-12 14:40:51 +0100

    prio

--- A3/priosched.c
+++ A3/priosched.c
@@ -2,39 +2,67 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
 
-double count(int max)
+double counter(long int max)
 {
-  int i = 0, j = 0;
-  time_t start, end;
-  double diff;
-  
-  time(&start);
-  for(i = 0; i < max; i++)
-  {   
-    j++;
-  }
-  time(&end);
-  diff = difftime(end, start);
-  
-  return diff;
+	struct timeval start, end;
+	long int i = 0, j = 0;
+	gettimeofday(&start, NULL);
+	for(i = 0; i < max; i++)
+	{
+		j = j + 1;
+	}
+	gettimeofday(&end, NULL);
+
+	double diff = ((double)end.tv_sec + ((double)end.tv_usec)/1000000) - (((double)start.tv_sec + ((double)start.tv_usec)/1000000));
+	return diff;
 }
 
-int main(void)
-{
-  int i = 0;
-  int status = 0;
-  printf("parent\n");
-  for(i = 0; i < 10; i++)
-  {
-    pid_t pid = fork();
-    
-    if(pid == 0)
-    {
-      printf("child");
-      printf("%s%f\n", "Time: ", count(100000000)); 
-      exit(0);
-    }
-    wait(&status);
-  }
+
+ int forking(int i)
+ {
+   	int status;
+   	pid_t pid;
+      
+   	pid = fork ();
+   
+	if(pid == 0)
+   	{	
+   		int which = PRIO_PROCESS;
+   		id_t id = getpid();
+   		setpriority(which, id, i);
+		//printf("%s%d%s%d\n", "Child: ", i, "PID: ",getpid());
+		//counter(100000000000000);
+		while(1)
+		{
+			printf("%s%d%s%f\n", "Child: ", i, "Time: ", counter(1000000));
+		}
+    	exit(EXIT_SUCCESS);
+   	}
+   
+   	else if(pid < 0)
+   	{
+    	 /* The fork failed */
+     	//printf("Failed to fork(): %s\n", command);
+     	status = -1;
+   	}	
+   	/*else
+     	if(waitpid(pid, &status, 0) != pid)
+       		status = -1;  */
+  	return status;
+}
+ 
+int main(int argc, char *argv[])
+{ 
+	int i;
+ 
+	/* Loop through argv */
+	for(i = 1; i < 10; i++)
+    	forking(i);
+
+    sleep(100);
+ 
+   	return 0;
 }
--- A3/readwritelock.c
+++ A3/readwritelock.c
@@ -1,27 +1,36 @@
 #include "readwritelock.h"
 
-struct rwlock *rwl;
+struct rwlock rwl;
 int i = 0;
 
 void *thread(void *vargp)
 {
-	/*puts("test");
-	rwlock_lockWrite(rwl);
-	puts("test1");
-	i = i + 1;
-	rwlock_unlock(rwl);*/
-	pthread_exit(0);
+	rwlock_lockWrite(&rwl);
+	int k = 0;
+	for(k = 0; k < 10; k++)
+	{
+		i = i + 1;
+	}
+	rwlock_unlock(&rwl);
+
+	printf("%d\n", i);
+	return NULL;
 }
 
 int main(void)
 {	
-	rwlock_init(rwl);
+	rwlock_init(&rwl);
+
 	int j = 0;
-	pthread_t threads[100]; 
-	for(j = 0; j < 100; j++)
+	pthread_t threads[10]; 
+	int thread_args[10];
+	for(j = 0; j < 10; j++)
 	{
-		pthread_create(&threads[i], NULL, thread, NULL);
-        pthread_join(threads[i],NULL); 
+		pthread_create(&threads[i], NULL, thread, &thread_args[j]);
+	}
+	for(j = 0; j < 10; j++)
+	{
+	    pthread_join(threads[i],NULL); 
 	}
 	return 1;
 }
@@ -31,11 +40,13 @@ struct rwlock
 {
 	pthread_mutex_t readmutex;
 	pthread_mutex_t writemutex;
+	pthread_cond_t writecondition;
+	pthread_cond_t readcondition;
 };
 
 void rwlock_init(struct rwlock *lock)
 {
-	puts("initing");
+	//struct rwlock ret;
 	if(pthread_mutex_init(&(lock->readmutex), 0) < 0)
 	{
 		perror("Init of readmutex failed!");
@@ -45,6 +56,16 @@ void rwlock_init(struct rwlock *lock)
 	{
 		perror("Init of writemutex failed!");
 	}
+	if(pthread_cond_init(&(lock->writecondition), 0) < 0)
+	{
+		perror("Init of writecondition failed!");
+	}
+	if(pthread_cond_init(&(lock->readcondition), 0) < 0)
+	{
+		perror("Init of readcondition failed!");
+	}
+	
+	//lock = &ret;
 	puts("initied");
 }
 
